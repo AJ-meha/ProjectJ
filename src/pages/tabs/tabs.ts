9bdb2c7a8ca39d6db7c4fc39f5a5d0e6eacf91e5
jeromie <jeromie@ajency.in>
2018-03-01 17:28:42 +0530

    ionic setup

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,17 +1,19 @@
 import { Component } from '@angular/core';
 
-import { AboutPage } from '../about/about';
-import { ContactPage } from '../contact/contact';
-import { HomePage } from '../home/home';
+import { FavouritePage } from '../favourite/favourite';
+import { MessagePage } from '../message/message';
+import { SearchPage } from '../search/search';
+import { ProfilePage } from '../profile/profile';
 
 @Component({
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
 
-  tab1Root = HomePage;
-  tab2Root = AboutPage;
-  tab3Root = ContactPage;
+  tab1Root = SearchPage;
+  tab2Root = FavouritePage;
+  tab3Root = MessagePage;
+  tab4Root = ProfilePage;
 
   constructor() {
 
