a6a5b06963510522029829a3169c36b6a9f0f023
jeromie <jeromie@ajency.in>
2018-03-06 20:41:51 +0530

    add job

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { MessagePage } from '../pages/message/message';
 import { SearchPage } from '../pages/search/search';
 import { ProfilePage } from '../pages/profile/profile';
 import { TabsPage } from '../pages/tabs/tabs';
+import { AddJobPage } from '../pages/add-job/add-job';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -19,7 +20,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     MessagePage,
     SearchPage,
     ProfilePage,
-    TabsPage
+    TabsPage,
+    AddJobPage
   ],
   imports: [
     BrowserModule,
@@ -32,7 +34,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     MessagePage,
     SearchPage,
     ProfilePage,
-    TabsPage
+    TabsPage,
+    AddJobPage
   ],
   providers: [
     StatusBar,
