5fa5ccf5669b44e861bf52703e41ef5de1b39f66
Viraj Verlekar <viraj@ajency.in>
2018-03-07 17:48:08 +0530

    Session mangement

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,14 +5,27 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { TabsPage } from '../pages/tabs/tabs';
 import { AdminLoginPage } from '../pages/admin-login/admin-login';
+import { AdminDashboardPage } from '../pages/admin-dashboard/admin-dashboard';
+
+import { AngularFireAuth } from 'angularfire2/auth';
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = AdminLoginPage;
+  rootPage:any;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, afAuth: AngularFireAuth) {
+    const authObserver=afAuth.authState.subscribe(user=>{
+      if(user){
+        this.rootPage=AdminDashboardPage;
+        authObserver.unsubscribe();
+      }
+      else{
+        this.rootPage=AdminLoginPage;
+        authObserver.unsubscribe();
+      }
+    });
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
