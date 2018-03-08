f40df2f2034b05c28d4d3b415231dd572f584fac
Viraj Verlekar <viraj@ajency.in>
2018-03-08 11:42:09 +0530

    Side Navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { Platform, Nav } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
@@ -13,9 +13,15 @@ import { AngularFireAuth } from 'angularfire2/auth';
   templateUrl: 'app.html'
 })
 export class MyApp {
+  @ViewChild(Nav) nav: Nav;
   rootPage:any;
+  pages: Array<{title:string,component:any}>;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, afAuth: AngularFireAuth) {
+    this.pages=[
+      {title:'Dashboard',component:AdminDashboardPage}
+    ];
+
     const authObserver=afAuth.authState.subscribe(user=>{
       if(user){
         this.rootPage=AdminDashboardPage;
@@ -26,11 +32,18 @@ export class MyApp {
         authObserver.unsubscribe();
       }
     });
+
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
     });
+  }
+
+  openPage(page) {
+    // Reset the content nav to have just this page
+    // we wouldn't want the back button to show in this scenario
+    this.nav.setRoot(page.component);
   }
 }
