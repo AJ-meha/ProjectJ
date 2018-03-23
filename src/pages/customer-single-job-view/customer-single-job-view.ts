50257f3179e8246d8c59926a5b6aef475eeb3cf7
Tanvi Salkar <tanvi.s@ajency.in>
2018-03-23 17:48:06 +0530

    changes

--- src/pages/customer-single-job-view/customer-single-job-view.ts
+++ src/pages/customer-single-job-view/customer-single-job-view.ts
@@ -22,9 +22,11 @@ export class CustomerSingleJobViewPage {
   public id:string;
   public savedJobsRef: firebase.database.Reference = firebase.database().ref('jobs');
   public empTypeRef: firebase.database.Reference = firebase.database().ref('employment_type');
+  public typeRef: firebase.database.Reference = firebase.database().ref('type');
   public dbRef: firebase.database.Reference = firebase.database().ref();
   public job: Array<any> = [];
   public empTypes: Array<any> = [];
+  public types: Array<any> = [];
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public viewCtrl: ViewController) {
     console.log("id="+ this.navParams.get('id'))
@@ -58,6 +60,7 @@ export class CustomerSingleJobViewPage {
       });
     });
 
+
     this.dbRef.child('jobs/').child(this.id).once('value').then( function(itemSnap) {
           console.log(itemSnap.val())
           let job_details_id=itemSnap.val().job_details_id
@@ -65,6 +68,9 @@ export class CustomerSingleJobViewPage {
           let designation='';
           let employment_type='';
           let salary_amount='';
+          let type='';
+          let industry='';
+          let sub_industry='';
           // var userId = snapshot.val().userId; // line 1 (results like 1,2,3,4,5,6)
           // console.log("job_details_id=="+job_details_id)
           // console.log(this.jobDetailsRef.child('job_details').child(job_details_id).val())
@@ -73,16 +79,29 @@ export class CustomerSingleJobViewPage {
               // console.log(mediaSnap.val());
               designation=mediaSnap.val().designation
               employment_type=mediaSnap.val().employment_type
+              type=mediaSnap.val().type
               salary_amount=mediaSnap.val().salary_amount
+              industry=mediaSnap.val().industry
+              sub_industry=mediaSnap.val().sub_industry
 
               self.dbRef.child('jobs_contact_workplace/').child(job_contact_workplace_id).once('value').then( function(jobContactSnap) {
                 self.job['key']=itemSnap.key;
                 self.job['value']=itemSnap.val();
                 self.job['designation']=designation;
+                
                 self.job['employment_type']=employment_type;
                 self.job['workplace_name']=jobContactSnap.val().workplace_name
                 self.job['workplace_address']=jobContactSnap.val().workplace_address
-                self.job['salary_amount']=salary_amount
+                self.job['salary_amount']=salary_amount;
+                self.dbRef.child('type/').child(type).once('value').then( function(typesSnap) {
+                  self.job['type']=typesSnap.val().text;
+                });
+                self.dbRef.child('industry/').child(industry).once('value').then( function(typesSnap) {
+                  self.job['industry']=typesSnap.val();
+                });
+                self.dbRef.child('industry_subindustry/').child(industry).child(sub_industry).once('value').then( function(subtypesSnap) {
+                  self.job['sub_industry']=subtypesSnap.val();
+                });
               });
               
               
