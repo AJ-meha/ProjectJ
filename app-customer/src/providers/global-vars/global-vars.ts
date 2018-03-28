22055d04b59fd9bbf523ce36fbdd42c2ffffeb35
Viraj Verlekar <viraj@ajency.in>
2018-03-28 15:15:03 +0530

    Mobile code dropdown - customer

--- app-customer/src/providers/global-vars/global-vars.ts
+++ app-customer/src/providers/global-vars/global-vars.ts
@@ -9,12 +9,12 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class GlobalVarsProvider {
-  static mobile_code_arr:[{key:"+91",value:"+ 91"},{key:"+852",value:"+ 852"}];
   /* GLOBAL constants,variable to be declared here*/
   static from_email: string= "jobsproject2018@gmail.com";
   static daysArray = [{key:"mon",value:"MON"},{key:"tue",value:"TUE"},{key:"wed",value:"WED"},{key:"thu",value:"THU"},{key:"fri",value:"FRI"},{key:"sat",value:"SAT"},{key:"sun",value:"SUN"}];
   static max_option = 4;
-  static mobile_code: string= "+ 91";
+  //static mobile_code: string= "+91";
+  static mobile_code = [{key:"+91",value:"+91"},{key:"+852",value:"+852"}];
 
   constructor() {
   }
