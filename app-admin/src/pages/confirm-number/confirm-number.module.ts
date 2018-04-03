b984aa83306e6507e2bbdc74fdca117335af6c8e
jeromie <jeromie@ajency.in>
2018-04-03 17:50:24 +0530

    tel number modal

---
+++ app-admin/src/pages/confirm-number/confirm-number.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { ConfirmNumberPage } from './confirm-number';
+
+@NgModule({
+  declarations: [
+    ConfirmNumberPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(ConfirmNumberPage),
+  ],
+})
+export class ConfirmNumberPageModule {}
